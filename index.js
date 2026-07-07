b5d016f9eb5ab8a9b3cd91ef4e265930fc576884
RyanKibz <ryan.serem@students.moringaschool.com>
2026-07-07 19:49:37 +0300

    The final code.

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+require('datejs');
 
+function combineUsers(...args) {
+
+  // Create the return object
+  const combinedObject = {
+    users: []
+  };
+
+  // Merge all arrays into one
+  for (let array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
+
+  // Add today's date
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Return the object
+  return combinedObject;
+}
 
 
 module.exports = {
